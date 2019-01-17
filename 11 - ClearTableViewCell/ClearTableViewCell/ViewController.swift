7e5c3cb4528c7ddaf8a66eb6d57ae891dce223b0
halseywu <wsh940808@163.com>
2019-01-17 16:43:51 +0800

    完成了11，但是没有什么意思

--- 11 - ClearTableViewCell/ClearTableViewCell/ViewController.swift
+++ 11 - ClearTableViewCell/ClearTableViewCell/ViewController.swift
@@ -8,12 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        cell.textLabel?.text = "你有事吗"
+        cell.textLabel?.backgroundColor = UIColor.clear
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: "cell")
     }
+    
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.backgroundColor = UIColor.red
+    }
+    
 
 
 }
