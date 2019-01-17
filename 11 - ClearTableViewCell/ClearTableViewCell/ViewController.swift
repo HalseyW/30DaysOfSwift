1a4a98a547005cea6adf516b24d9ffbd5a27bc4c
halseywu <wsh940808@163.com>
2019-01-17 14:44:19 +0800

    10 已完成，创建了 11 项目

---
+++ 11 - ClearTableViewCell/ClearTableViewCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClearTableViewCell
+//
+//  Created by HalseyW-15 on 2019/1/17.
+//  Copyright © 2019 wushhhhhh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
