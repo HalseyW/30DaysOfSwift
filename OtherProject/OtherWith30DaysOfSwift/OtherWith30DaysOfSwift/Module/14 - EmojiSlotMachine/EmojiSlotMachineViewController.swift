09141bb44ccf4ea00eaff33bfffaa561c2e9dacc
halseywu <wsh940808@163.com>
2019-07-10 10:09:30 +0800

    完成14

--- OtherProject/OtherWith30DaysOfSwift/OtherWith30DaysOfSwift/Module/14 - EmojiSlotMachine/EmojiSlotMachineViewController.swift
+++ OtherProject/OtherWith30DaysOfSwift/OtherWith30DaysOfSwift/Module/14 - EmojiSlotMachine/EmojiSlotMachineViewController.swift
@@ -5,11 +5,20 @@
 //  Created by HalseyW-15 on 2019/7/9.
 //  Copyright © 2019 wushhhhhh. All rights reserved.
 //
+//知识点：
+//随机数获取：
+// [0,100) -> arc4random() % 100
+// [500,1000) -> (arc4random() % 501) + 500
+// X ~ N 之间的随机数: arc4random() % (N - X) + X
 
 import UIKit
 
 class EmojiSlotMachineViewController: UIViewController {
     let pickerView = UIPickerView()
+    let emojis = ["👻","👸","💩","😘","🍔","🤖","🍟","🐼","🚖","🐷"]
+    var data1 = [Int]()
+    var data2 = [Int]()
+    var data3 = [Int]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +33,34 @@ class EmojiSlotMachineViewController: UIViewController {
             make.height.equalTo(300)
             make.center.equalToSuperview()
         }
+        
+        let btnSelect = UIButton()
+        btnSelect.backgroundColor = .lightGray
+        btnSelect.setTitle("SELECT", for: .normal)
+        self.view.addSubview(btnSelect)
+        btnSelect.snp.makeConstraints { (make) in
+            make.width.equalToSuperview().inset(30)
+            make.height.equalTo(50)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(pickerView.snp.bottom).offset(30)
+        }
+        btnSelect.addTarget(self, action: #selector(onClickSelectButton(_:)), for: .touchUpInside)
+        
+        for _ in 0 ... 100 {
+            data1.append(Int.random(in: 0 ... 9))
+            data2.append(Int.random(in: 0 ... 9))
+            data3.append(Int.random(in: 0 ... 9))
+        }
+    }
+    
+    @objc func onClickSelectButton(_ sender: UIButton) {
+        let randomRow1 = Int.random(in: 0 ... 99)
+        let randomRow2 = Int.random(in: 0 ... 99)
+        let randomRow3 = Int.random(in: 0 ... 99)
+
+        pickerView.selectRow(randomRow1, inComponent: 0, animated: true)
+        pickerView.selectRow(randomRow2, inComponent: 1, animated: true)
+        pickerView.selectRow(randomRow3, inComponent: 2, animated: true)
     }
 
 }
@@ -38,7 +75,24 @@ extension EmojiSlotMachineViewController: UIPickerViewDelegate, UIPickerViewData
     }
     
     func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
-        return 60
+        return 80
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let lable = UILabel()
+        switch component {
+        case 0:
+            lable.text = emojis[data1[row]]
+        case 1:
+            lable.text = emojis[data2[row]]
+        case 2:
+            lable.text = emojis[data3[row]]
+        default:
+            break
+        }
+        lable.font = UIFont.systemFont(ofSize: 60)
+        lable.textAlignment = .center
+        return lable
     }
     
 }
